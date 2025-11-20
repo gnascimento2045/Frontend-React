95587f458b27e30a69e9f9e07199568279a2e70b
GabrielPC <kira.msd10@gmail.com>
2025-11-20 03:02:21 -0300

    Criado rotas e configuração da API

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,52 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Apps/pages/Login";
-import Register from "./Apps/pages/Login/Register";
-import Dashboard from "./Apps/pages/Dashboard";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import { ThemeProvider } from './contexts/ThemeContext'
+import PrivateRoute from './components/PrivateRoute'
+import Layout from './components/Layout/Layout'
+
+// Pages
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import Dashboard from './pages/Dashboard/Dashboard'
+import PDV from './pages/PDV/PDV'
+import Products from './pages/Products/Products'
+import Customers from './pages/Customers/Customers'
+import Sales from './pages/Sales/Sales'
+import CashRegister from './pages/CashRegister/CashRegister'
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <ThemeProvider>
+        <AuthProvider>
+          <Routes>
+            {/* Public routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* Private routes */}
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Layout />
+                </PrivateRoute>
+              }
+            >
+              <Route index element={<Navigate to="/dashboard" replace />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="pdv" element={<PDV />} />
+              <Route path="produtos" element={<Products />} />
+              <Route path="clientes" element={<Customers />} />
+              <Route path="vendas" element={<Sales />} />
+              <Route path="caixa" element={<CashRegister />} />
+            </Route>
+
+            {/* 404 */}
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+          </Routes>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
-  );
+  )
 }
